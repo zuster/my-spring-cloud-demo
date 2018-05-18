72cb87c218132cb3013be94a7edcc6095342655d
e <zuster@sina.cn>
2018-05-19 02:17:01 +0800

    init commit

---
+++ cloud-provider-use/src/main/java/com/zuster/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.zuster.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.zuster.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+}
