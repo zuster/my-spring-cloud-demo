45b547aec780c2e694a6804cfa4b31e00156bb25
e <zuster@sina.cn>
2018-05-30 22:42:58 +0800

    ignore target

--- cloud-provider-use-1-2/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-insert into user(id,username,name,age,balance) values(1,'user1','张三',20,100.00);
-insert into user(id,username,name,age,balance) values(2,'user2','李四',30,200.00);
-insert into user(id,username,name,age,balance) values(3,'user3','王五',40,300.00);
-insert into user(id,username,name,age,balance) values(4,'user4','赵柳',50,400.00);
-insert into user(id,username,name,age,balance) values(5,'user5','李琪',60,500.00);
